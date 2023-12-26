1b3105a820e17cdfe3505590ffb4e24853bf5926
Lily Carroll <lily.carroll@stu.ocsb.ca>
2023-12-26 18:14:14 +0000

    Code is fine, just need to fixTry catch

--- assign_6.py
+++ assign_6.py
@@ -14,28 +14,18 @@ def elements_at_odd_positions(list_of_elements):
     # Declaring an empty list for odd elements.
     odd_element_list = []
 
-    # Checking if the input is already a list
-    if isinstance(list_of_elements, list):
-        # Using a For each loop to get the odd elements from the list.
-        for index, element in enumerate(list_of_elements):
-            # Checking if the element's index is odd.
-            if (index % 2 == 1):
-                # Appending the odd element to the list.
-                odd_element_list.append(element)
-    else:
-        # If the input is a string, split it and convert to integers
-        list_as_integer = [int(element) for element in list_of_elements.split()]
+    # Using a For each loop to get the odd elements from the list.
+    for index, element in enumerate(list_of_elements):
+        # Checking if the element's index is odd.
+        if (index % 2 == 1):
+            # Appending the odd element to the list.
+            odd_element_list.append(element)
 
-        # Using a For each loop to get the odd elements from the list.
-        for index, element in enumerate(list_as_integer):
-            # Checking if the element's index is odd.
-            if (index % 2 == 1):
-                # Appending the odd element to the list.
-                odd_element_list.append(element)
-
-    # Returning the odd elements in the list to the function.
+    # Returning the odd element in the list to the function.
     return odd_element_list
 
+# Creating a function that will display the individual digits inside
+# a number using lists.
 def list_of_numbers(whole_number):
     # Declaring a list to hold the individual elements.
     list_of_digits = []
@@ -46,13 +36,15 @@ def list_of_numbers(whole_number):
     # Using a For Each loop to get the individual elements.
     for digit in whole_number_str:
         # Appending the individual digits to the list.
+        # Also converting it into an integer.
         list_of_digits.append(int(digit))
 
     # Returning the list to the function.
     return list_of_digits
 
+# Main function that will allow for user input/output and also error check.
 def main():
-    # Getting user input.
+    # Getting user inputs.
     list_as_string = input("Please enter a string of integers separated by spaces: ")
     whole_number_as_string = input("Please enter a positive whole number: ")
 
@@ -60,22 +52,24 @@ def main():
     try:
         # Converting the user input as a string into a list of integers.
         list_as_integer = [int(element) for element in list_as_string.split()]
-
+        # Converting the user input from a string to an integer.
         whole_number_as_integer = int(whole_number_as_string)
 
-        # Checking if they entered a negative number.
+        # Checking if they entered a negative number and verifying numbers in list are positive.
         if whole_number_as_integer < 0 or any(num < 0 for num in list_as_integer):
             print("Please enter a positive number.")
+        # Else they're positive, so call functions.
         else:
             odd_elements = elements_at_odd_positions(list_as_integer)
             separated_digits = list_of_numbers(whole_number_as_integer)
 
-            # Displaying the digits of the user's number.
+            # Displaying the digits of the user's number and the
+            # elements at odd positions from their list.
             print("The elements in your list at odd positions are: {}.".format(odd_elements))
             print("The digits of your numbers are: {}.".format(separated_digits))
 
     # Catching any errors.
-    except ValueError:
+    except:
         print("Invalid input. Please try again")
 
 if __name__ == "__main__":
